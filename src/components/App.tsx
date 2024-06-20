fe83f980935d67843255c5339b3d8f0696dfe157
Ales Zima <ales@cpex.cz>
2024-06-20 17:20:15 +0200

    Added background img and header to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,27 @@
+import bgDesktopLight from '../assets/images/bg-desktop-light.jpg';
+import bgMobileLight from '../assets/images/bg-mobile-light.jpg';
+import moonIcon from '../assets/images/icon-moon.svg';
+
 export default function App() {
+  const isMobile = window.innerWidth < 768;
+
   return (
-    <div className="text-lg">
-      <h1>TODO</h1>
+    <div className="text-lg min-h-lvh bg-neutral-light-gray-100 text-neutral-light-gray-500">
+      <img
+        src={isMobile ? bgMobileLight.src : bgDesktopLight.src}
+        alt="Background image of mountains"
+        className="fixed top-0 z-0"
+      />
+      <div className="relative z-10 mx-auto max-w-xl py-20">
+        <div className="flex justify-between items-center">
+          <h1 className="text-neutral-light-gray-100 text-5xl tracking-[0.25em]">
+            TODO
+          </h1>
+          <button>
+            <img src={moonIcon.src} alt="Moon icon" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
