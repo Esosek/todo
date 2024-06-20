59379f6a9242c72383bc4f0ee4be66e9f8c14006
Ales Zima <ales@cpex.cz>
2024-06-20 16:12:33 +0200

    Added colors to Tailwind theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,31 @@ export default {
     fontFamily: {
       sans: ['Josefin Sans', 'system-ui', 'sans-serif'],
     },
+    colors: {
+      primary: {
+        'blue-500': 'hsl(220, 98%, 61%)',
+        'blue-gradient-start': 'hsl(192, 100%, 67%)',
+        'blue-gradient-end': 'hsl(280, 87%, 65%)',
+      },
+      neutral: {
+        light: {
+          'gray-100': 'hsl(0, 0%, 98%)',
+          'gray-200': 'hsl(236, 33%, 92%)',
+          'gray-300': 'hsl(233, 11%, 84%)',
+          'gray-400': 'hsl(236, 9%, 61%)',
+          'gray-500': 'hsl(235, 19%, 35%)',
+        },
+        dark: {
+          'gray-600': 'hsl(235, 21%, 11%)',
+          'gray-700': 'hsl(235, 24%, 19%)',
+          'dark-gray-200': 'hsl(236, 33%, 92%)',
+          'dark-gray-300': 'hsl(234, 39%, 85%)',
+          'dark-gray-500': 'hsl(234, 11%, 52%)',
+          'dark-gray-600': 'hsl(233, 14%, 35%)',
+          'dark-gray-700': 'hsl(237, 14%, 26%)',
+        },
+      },
+    },
     extend: {},
   },
   plugins: [],
