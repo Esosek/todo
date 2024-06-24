3e1e87be3370db5249cfa1fb71c97574bbae7b17
Ales Zima <ales@cpex.cz>
2024-06-24 17:42:26 +0200

    Added list filtering

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,35 +1,87 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import TodoContext, { TodoItemStatus } from '../context/TodoContext';
 import TodoListItem from './TodoListItem';
 
 export default function TodoList() {
+  enum Filter {
+    ALL = 'All',
+    ACTIVE = 'Active',
+    COMPLETED = 'Completed',
+  }
+
   const todoContext = useContext(TodoContext);
+  const [filter, setFilter] = useState(Filter.ALL);
+  let [filteredTodos, setFilteredTodos] = useState(todoContext.items);
+
+  useEffect(() => {
+    switch (filter) {
+      case Filter.ALL:
+        setFilteredTodos(
+          todoContext.items.filter(
+            (item) => item.status !== TodoItemStatus.DELETED
+          )
+        );
+        break;
+      case Filter.ACTIVE:
+        setFilteredTodos(
+          todoContext.items.filter(
+            (item) => item.status === TodoItemStatus.ACTIVE
+          )
+        );
+        break;
+      case Filter.COMPLETED:
+        setFilteredTodos(
+          todoContext.items.filter(
+            (item) => item.status === TodoItemStatus.COMPLETED
+          )
+        );
+        break;
+      default:
+        setFilteredTodos(todoContext.items);
+        break;
+    }
+  }, [filter, todoContext.items]);
 
   const activeTodosLenght = todoContext.items.filter(
     (item) => item.status === TodoItemStatus.ACTIVE
   ).length;
 
+  function selectFilter(filter: Filter) {
+    setFilter(filter);
+  }
+
+  function handleClearCompleted() {
+    todoContext.clearCompleted();
+  }
+
   return (
     <div className="bg-neutral-light-gray-100 mt-6 rounded-md">
       <ul>
-        {todoContext.items.map((todo) => (
+        {filteredTodos.map((todo) => (
           <TodoListItem key={todo.id} todo={todo} />
         ))}
       </ul>
       <div className="grid grid-cols-3 py-3 px-6 text-base text-neutral-light-gray-400">
         <p>{activeTodosLenght} items left</p>
         <div className="flex gap-4 font-bold">
-          <button className="text-neutral-light-gray-400 hover:text-neutral-light-gray-500">
-            All
-          </button>
-          <button className="text-neutral-light-gray-400 hover:text-neutral-light-gray-500">
-            Active
-          </button>
-          <button className="text-neutral-light-gray-400 hover:text-neutral-light-gray-500">
-            Completed
-          </button>
+          {Object.values(Filter).map((f) => (
+            <button
+              key={f}
+              onClick={() => selectFilter(f)}
+              className={
+                f === filter
+                  ? 'text-primary-blue-500'
+                  : 'text-neutral-light-gray-400 hover:text-neutral-light-gray-500'
+              }
+            >
+              {f}
+            </button>
+          ))}
         </div>
-        <button className="text-right hover:text-neutral-light-gray-500">
+        <button
+          onClick={handleClearCompleted}
+          className="text-right hover:text-neutral-light-gray-500"
+        >
           Clear Completed
         </button>
       </div>
