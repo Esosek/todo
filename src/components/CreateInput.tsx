70813602d5e2aa72b624de2f06c6a5fcdab73582
Ales Zima <ales@cpex.cz>
2024-06-24 17:52:44 +0200

    Added adding new todos

--- src/components/CreateInput.tsx
+++ src/components/CreateInput.tsx
@@ -1,14 +1,33 @@
+import { useContext, useRef, type FormEvent } from 'react';
+import TodoContext from '../context/TodoContext';
+
 export default function CreateInput() {
+  const addTodo = useContext(TodoContext).addItem;
+  const inputElement = useRef<HTMLInputElement>(null);
+
+  function handleFormSubmit(e: FormEvent) {
+    e.preventDefault();
+    const userInput = inputElement.current?.value;
+    if (userInput && userInput.length > 1) {
+      addTodo(userInput);
+      inputElement.current.value = '';
+    }
+  }
+
   return (
-    <div className="flex items-center gap-6 bg-neutral-light-gray-100 rounded-md overflow-clip pl-6">
+    <form
+      onSubmit={handleFormSubmit}
+      className="flex items-center gap-6 bg-neutral-light-gray-100 rounded-md overflow-clip pl-6"
+    >
       <div className="size-6 border-2 rounded-full border-neutral-light-gray-200"></div>
       <input
         type="text"
         name="new-todo-input"
         id="new-todo-input"
         placeholder="Create a new todo..."
+        ref={inputElement}
         className="bg-neutral-light-gray-100 py-4 flex-grow focus:outline-none"
       />
-    </div>
+    </form>
   );
 }
